9d42fbef52fc86bdb505fbf3314e3cabd8ad545e
Robert Swan <mammal.robert@gmail.com>
2015-12-21 11:14:00 +0000

    painless upgrade to VS2015 complete

--- saves/MyWorld/default/settings.lua
+++ saves/MyWorld/default/settings.lua
@@ -1,16 +1,16 @@
 -- repeatedly generated file. not safe to hand edit
 local roomSettings =
 {
-	path = "default/",
-	blah1 = 0.003,
-	octavesMax = 5,
-	blah2 = 0,
 	frequencyPerOctave = 2,
-	scale = 128,
+	octavesMax = 5,
+	path = "default/",
+	randomSeedAsString = 0,
 	blah4 = 0,
 	blah3 = 1,
-	randomSeedAsString = 0,
+	blah2 = 0,
+	blah1 = 0.003,
 	amplitudePerOctave = 0.5,
+	scale = 128,
 }
 
 return roomSettings
