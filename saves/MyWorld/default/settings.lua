2d9ea5f4fb5ed1f1d752f1524da311fafe849c4a
Robert Swan <mammal.robert@gmail.com>
2015-12-21 12:25:52 +0000

    added lua check for alt-tabbing

--- saves/MyWorld/default/settings.lua
+++ saves/MyWorld/default/settings.lua
@@ -1,16 +1,16 @@
 -- repeatedly generated file. not safe to hand edit
 local roomSettings =
 {
-	frequencyPerOctave = 2,
+	blah2 = 0,
 	octavesMax = 5,
 	path = "default/",
-	randomSeedAsString = 0,
-	blah4 = 0,
-	blah3 = 1,
-	blah2 = 0,
-	blah1 = 0.003,
-	amplitudePerOctave = 0.5,
 	scale = 128,
+	blah4 = 0,
+	amplitudePerOctave = 0.5,
+	randomSeedAsString = 0,
+	blah1 = 0.003,
+	blah3 = 1,
+	frequencyPerOctave = 2,
 }
 
 return roomSettings
