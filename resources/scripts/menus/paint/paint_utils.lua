952bdfd8040f59e719eccd80c83a02b3f0037e75
Robert Swan <mammal.robert@gmail.com>
2016-01-27 10:30:54 +0000

    server side validation of player name length. refuse handshake works. added paint!

---
+++ resources/scripts/menus/paint/paint_utils.lua
@@ -0,0 +1,77 @@
+--------------------------------------------------
+local d = {}
+
+--------------------------------------------------
+function d.savePaintFile (canvas, fileName, h, w)
+    if fileName == nil or fileName == ".brs" then
+        return -1
+    end
+
+    local file = io.open (fileName, "w")
+
+    if file == nil then
+        return -2
+    end
+
+    local emptyFile = true
+
+    for rowIdx = 1, h do
+		for colIdx = 1, w do
+            local color = canvas [rowIdx][colIdx]
+
+            if emptyFile and color ~= 1 then
+                emptyFile = false
+            end
+
+            file:write (tostring (color))
+            file:write (" ")
+        end
+
+        if rowIdx ~= h then
+            file:write ("\n")
+        end
+    end
+
+    file:close()
+
+    if emptyFile then
+        return 2
+    end
+
+    return 1
+end
+
+--------------------------------------------------
+function d.loadPaintFile (fileName)
+    if fileName == nil or fileName == ".brs" then
+        return -1
+    end
+
+    local file = io.open (fileName, "r")
+
+    if file == nil then
+        return -2
+    end
+
+    local canvas = {}
+    local h = 1
+    local w = 1
+    local lines = file:lines ()
+
+    for line in lines do
+        canvas [h] = {}
+
+        for num in string.gmatch (line, "%S+") do
+            canvas [h][w] = tonumber (num)
+            w = w + 1
+        end
+
+        w = 1
+        h = h + 1
+    end
+
+    file:close ()
+    return canvas
+end
+
+return d
