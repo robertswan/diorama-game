0e9e64cdb1778c9b082cbe388f39fc110b9ca2fd
Robert Swan <mammal.robert@gmail.com>
2016-04-12 14:50:02 +0100

    added default terrain generator file for servers without a world specified

---
+++ resources/defaults/default_new_world_settings.lua
@@ -0,0 +1,41 @@
+-- repeatedly generated file. not safe to hand edit
+local roomSettings =
+{
+	generators = 
+	{
+		{
+			voxelPass = 
+			{
+				{
+					chanceOfTree = 0.03,
+					sizeMin = 3,
+					sizeRange = 3,
+					trunkHeight = 5,
+					type = "addTrees",
+				},
+				
+				{
+					mudHeight = 4,
+					type = "addGrass",
+				},
+			},
+			weightPass = 
+			{
+				
+				{
+					mode = "lessThan",
+					octaves = 4,
+					perOctaveAmplitude = 0.5,
+					perOctaveFrequency = 2,
+					scale = 64,
+					type = "perlinNoise",
+				},
+			},
+		},
+	},
+	path = "default/",
+	randomSeedAsString = "Teazel",
+	terrainId = "paramaterized",
+}
+
+return roomSettings
