dd33b7f1caea32114a0e86dbc0e696b295c77c85
Robert Swan <mammal.robert@gmail.com>
2016-07-04 11:09:22 +0100

    added default client settings for glfw and opengl

---
+++ resources/defaults/default_client_settings.lua
@@ -0,0 +1,22 @@
+local clientSettings =
+{
+    video =
+    {   
+        monitorIdx = 0,
+        swapInterval = 1,
+        window = {x = 100, y = 100, w = 1600, h = 800},
+        hasBorders = true,
+    },
+    audio =
+    {
+        mainVolume = 1.0,
+    },
+    openGl =
+    {
+        isCoreProfile = true,
+        majorVersion = 3,
+        minorVersion = 3,
+    },
+}
+
+return clientSettings
