b31043eb92a0eadab12abe36c914a918e97c9192
stewartconnor <sunbeat3@gmail.com>
2016-07-05 06:48:48 +1000

    Added emotes
    - There's some dupe code that I couldn't figure out how to fit nicely.
    Sorry about that.

--- resources/mods/diorama/chat/ticker_line.lua
+++ resources/mods/diorama/chat/ticker_line.lua
@@ -1,5 +1,6 @@
 --------------------------------------------------
 local Mixin = require ("resources/mods/diorama/frontend_menus/mixin")
+local EmoteDefinitions = require ("resources/mods/diorama/chat/emote_definitions")
 
 --------------------------------------------------
 local params =
@@ -7,6 +8,9 @@ local params =
     visibleDuration = 60 * 5,
     fadeOutDuration = 60 * 0.5,
 }
+
+local emotes = EmoteDefinitions.emotes
+local emoteSpecs = EmoteDefinitions.emoteSpecs
 
 --------------------------------------------------
 local c = {}
@@ -25,18 +29,70 @@ function c:update ()
 end
 
 --------------------------------------------------
+-- DUPE CODE
+local function getEmoteID (text)
+    -- Returns the emote ID or -1 if unsucessful
+    local emoteID = -1
+
+    if string.sub (text, 1, 1) == emoteSpecs.emoteStartChar then
+        local newString = string.sub (text, 2, string.len(text))
+
+        local i = 1
+        while emotes[i] ~= nil do
+            local emoteName = emotes[i].name
+            if newString == emoteName then
+                emoteID = i
+            end
+            i = i + 1
+        end
+
+    end
+
+    return emoteID
+end
+
+--------------------------------------------------
+-- DUPE CODE
+local function renderChatLine (self, line, y)
+    
+    local drawString = dio.drawing.font.drawString
+
+    drawString (0, y, line.author, 0x000000ff, true)
+    drawString (0, y + 2, line.author, 0xffff00ff, true)
+
+    local i = 1;
+    local x = self.textOffset
+
+    while line.textList[i] ~= nil do
+        local curText = line.textList[i]
+        local emoteID = getEmoteID(curText)
+
+        if emoteID > 0 then
+            -- draw the emote
+            local u, v = emotes[emoteID].uvs[1], emotes[emoteID].uvs[2]
+            dio.drawing.drawTextureRegion2 (self.emoteTexture,      x, y, 
+                                            emoteSpecs.renderWidth, emoteSpecs.renderHeight, 
+                                            u * emoteSpecs.width,   v * emoteSpecs.height, 
+                                            emoteSpecs.width,       emoteSpecs.height)
+            x = x + emoteSpecs.renderWidth
+        else
+            -- draw the string (with shadow)
+            drawString (x, y, curText, 0x000000ff, true)
+            drawString (x, y+2, curText, 0xffffffff, true)
+            x = x + dio.drawing.font.measureString (line.textList[i])
+        end 
+
+        i = i + 1
+    end
+end
+
+--------------------------------------------------
 function c:earlyRender ()
 
     dio.drawing.setRenderToTexture (self.renderToTexture)
 
-    dio.drawing.font.drawBox (0, 0, self.w, self.h, 0x000000b0);
-
-    local drawString = dio.drawing.font.drawString
-
-    drawString (0, 0, self.author, 0x000000ff)
-    drawString (0, 2, self.author, 0xffff00ff)
-    drawString (self.textOffset, 0, self.text, 0x000000ff, true)
-    drawString (self.textOffset, 2, self.text, 0xffffffff)
+    dio.drawing.font.drawBox (0, 0, self.w, self.h, 0x000000b0)
+    renderChatLine (self, self.line, 0)
 
     dio.drawing.setRenderToTexture (nil)
 
@@ -52,12 +108,11 @@ function c:lateRender (x, y)
 end
 
 --------------------------------------------------
-return function (author, text, w, h, textOffset)
+return function (line, w, h, textOffset, emoteTexture)
     
     local instance = 
     {
-        author = author,
-        text = text,
+        line = line,
         textOffset = textOffset,
         w = w,
         h = h,
@@ -65,6 +120,7 @@ return function (author, text, w, h, textOffset)
         alpha = 1,
         tickCount = 0,
         renderToTexture = dio.drawing.createRenderToTexture (w, h),
+        emoteTexture = emoteTexture, 
         isDirty = true,
     }
 
