e9090f225525e24e871031f4fb65a08f5c09fd85
Robert Swan <mammal.robert@gmail.com>
2016-08-22 15:10:39 +0100

    readd tiny galaxy creative mode

---
+++ resources/gamemodes/tiny_galaxy_creative/mods/world_logic/main_client.lua
@@ -0,0 +1,48 @@
+--------------------------------------------------
+local instance = nil
+
+--------------------------------------------------
+local function onLoad ()
+
+    dio.resources.loadTexture ("CHUNKS_DIFFUSE",    "textures/chunks_diffuse_tiny_galaxy.png")
+    dio.resources.loadTexture ("LIQUIDS_DIFFUSE",   "textures/liquids_diffuse_00.png")
+    dio.resources.loadTexture ("SKY_COLOUR",        "textures/sky_colour_tiny_galaxy.png", {isNearest = false})
+
+end
+
+--------------------------------------------------
+local function onUnload ()
+
+    dio.resources.destroyTexture ("CHUNKS_DIFFUSE")
+    dio.resources.destroyTexture ("LIQUIDS_DIFFUSE")
+    dio.resources.destroyTexture ("SKY_COLOUR")
+
+end
+
+--------------------------------------------------
+local modSettings =
+{
+    description =
+    {
+        id = "creative",
+        name = "Creative",
+        description = "This is required to play the game!",
+        help =
+        {
+        },
+    },
+
+    permissionsRequired =
+    {
+        resources = true,
+    },
+
+    callbacks =
+    {
+        onLoad = onLoad,
+        onUnload = onUnload,
+    }
+}
+
+--------------------------------------------------
+return modSettings
