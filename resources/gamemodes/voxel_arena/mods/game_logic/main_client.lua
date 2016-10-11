6d7863b0284a4b1f09f614fedda97664522836d9
Robert Swan <mammal.robert@gmail.com>
2016-10-11 11:09:58 +0100

    voxel arena now has an extruded texture

--- resources/gamemodes/voxel_arena/mods/game_logic/main_client.lua
+++ resources/gamemodes/voxel_arena/mods/game_logic/main_client.lua
@@ -108,10 +108,14 @@ local function onLoad ()
     dio.resources.loadTexture ("LIQUIDS_DIFFUSE",   "textures/liquids_diffuse_00.png")
     dio.resources.loadTexture ("SKY_COLOUR",        "textures/sky_colour_00.png", {isNearest = false})
 
+    dio.resources.loadExtrudedTexture ("CHUNKS_EXTRUDED",    "textures/chunks_extruded_00.png")
+
 end
 
 --------------------------------------------------
 local function onUnload ()
+
+    dio.resources.destroyExtrudedTexture ("CHUNKS_EXTRUDED")
 
     dio.resources.destroyTexture ("CHUNKS_DIFFUSE")
     dio.resources.destroyTexture ("LIQUIDS_DIFFUSE")
