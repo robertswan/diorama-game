557cb0f6175b6beaaa9c6876be4b18f4af10fe82
Robert Swan <mammal.robert@gmail.com>
2016-11-24 23:15:38 +0000

    left/right button works with spanner.

--- resources/gamemodes/default/mods/blocks/main_server.lua
+++ resources/gamemodes/default/mods/blocks/main_server.lua
@@ -4,7 +4,7 @@ local BlockDefinitions = require ("resources/gamemodes/default/mods/blocks/block
 local blockCallbacks = {}
 
 --------------------------------------------------
-function blockCallbacks.spanner (event) 
+function blockCallbacks.spanner (event, isPlacing) 
 
     if event.isBlockValid then
         if BlockDefinitions.blocks [event.pickedBlockId].isMotor then
@@ -19,23 +19,38 @@ function blockCallbacks.spanner (event)
                 -- can we get the parent?
 
                 local c = dio.entities.components
-                
+
                 local motorEntityId = dio.entities.getComponent (event.chunkEntityId, c.PARENT).parentEntityId
-                local transform = dio.entities.getComponent (motorEntityId, c.TRANSFORM)
                 local rotatingMotor = dio.entities.getComponent (motorEntityId, c.ROTATING_MOTOR)
 
-                -- need to find its rotating speed, and then modify it ?
-                -- so lets hard code this, and toggle between rotating and off in X
-
-                if rotatingMotor.rotationSpeed [1] == 0 then
-                    transform.ypr [1] = -0.2
-                    rotatingMotor.rotationSpeed [1] = -0.2
+                if isPlacing then
+                    rotatingMotor.isActive = not rotatingMotor.isActive
                 else
-                    transform.ypr [1] = 0
-                    rotatingMotor.rotationSpeed [1] = 0
-                end
+                    if rotatingMotor.isActive then
+                        local rotationSpeedInc = 0.02
+                        local maxRotationSpeed = 0.2
 
-                dio.entities.setComponent (motorEntityId, c.TRANSFORM, transform)
+                        rotatingMotor.rotationSpeed [1] = rotatingMotor.rotationSpeed [1] + rotationSpeedInc
+                        if rotatingMotor.rotationSpeed [1] > maxRotationSpeed then
+                            rotatingMotor.rotationSpeed [1] = rotationSpeedInc
+                        end
+                    end
+                end
+                
+                -- -- local transform = dio.entities.getComponent (motorEntityId, c.TRANSFORM)
+
+                -- -- need to find its rotating speed, and then modify it ?
+                -- -- so lets hard code this, and toggle between rotating and off in X
+
+                -- if rotatingMotor.rotationSpeed [1] == 0 then
+                --     -- transform.ypr [1] = -0.2
+                --     rotatingMotor.rotationSpeed [1] = -0.2
+                -- else
+                --     -- transform.ypr [1] = 0
+                --     rotatingMotor.rotationSpeed [1] = 0
+                -- end
+
+                -- dio.entities.setComponent (motorEntityId, c.TRANSFORM, transform)
                 dio.entities.setComponent (motorEntityId, c.ROTATING_MOTOR, rotatingMotor)
             end
         end
@@ -49,14 +64,26 @@ local function onEntityPlaced (event)
 
     if event.isBlockValid then
 
-        local blockTag = BlockDefinitions.blocks [event.replacementBlockId].tag
+        local blockTag = BlockDefinitions.blocks [event.usingBlockId].tag
         if blockTag then
-            event.cancel = blockCallbacks [blockTag] (event)
+            event.cancel = blockCallbacks [blockTag] (event, true)
             return
         end
     end
 end
 
+--------------------------------------------------
+local function onEntityDestroyed (event)
+
+    if event.isBlockValid then
+
+        local blockTag = BlockDefinitions.blocks [event.usingBlockId].tag
+        if blockTag then
+            event.cancel = blockCallbacks [blockTag] (event, false)
+            return
+        end
+    end
+end
 
 --------------------------------------------------
 local function onLoad ()
@@ -69,7 +96,7 @@ local function onLoad ()
 
     local types = dio.types.serverEvents
     dio.events.addListener (types.ENTITY_PLACED, onEntityPlaced)
-    -- dio.events.addListener (types.ENTITY_DESTROYED, onEntityDestroyed)
+    dio.events.addListener (types.ENTITY_DESTROYED, onEntityDestroyed)
 
 end
 
